485a3111c216768b7582a943fcdbe15ad44beb74
Andrew4Aces <avm15031979@gmail.com>
2018-05-03 16:46:45 +0300

    SpaceShip rotate

---
+++ SpaceShip/Assets/Scripts/ShipController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour 
+{
+	Quaternion rotationZLeft = Quaternion.AngleAxis(1,new Vector3(0,0,1));
+	Quaternion rotationZRight = Quaternion.AngleAxis(-1,new Vector3(0,0,1));
+	void Start ()
+	{
+
+	}
+
+	void FixedUpdate ()
+	{
+		if(Input.GetKey(KeyCode.A))
+			{
+				if(transform.rotation.z <= 0.999f)
+					{
+						transform.rotation *= rotationZLeft;
+						Debug.Log(transform.rotation.z);
+					}
+			}
+		if(Input.GetKey(KeyCode.D))
+			{
+				if(transform.rotation.z >= 0.05f)
+				{
+					transform.rotation *= rotationZRight;
+				}	
+			}
+	}
+}
